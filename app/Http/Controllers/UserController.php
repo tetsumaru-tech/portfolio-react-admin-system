1eaa326877ad5b6ad2792bc223b3b289823c405f
Tetsuya Honda <create.a.fun.life@gmail.com>
2026-04-22 04:57:19 +0900

    Httpステータスを定義

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -37,7 +37,7 @@ class UserController extends Controller
     {
         $data = $request->validated();
 
-        return response()->json(['id' => 11, ...$data, 'createdAt' => now()->toIso8601String()]);
+        return response()->json(['id' => 11, ...$data, 'createdAt' => now()->toIso8601String()], 201);
     }
 
     public function update(UserRequest $request, int $id): JsonResponse
